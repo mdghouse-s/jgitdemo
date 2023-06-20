66939ec59dc924bd67a8176016bd4a28ee246e4c
Mohammed Ghouse <mohammed.shaikh@stackroute.in>
2023-06-20 16:04:37 +0530

    Committing files

---
+++ demo/src/main/java/com/test/RemotePush.java
@@ -0,0 +1,62 @@
+package com.test;
+
+
+//import required classes
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+
+//Add logger import
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class RemotePush {
+
+    //create logger
+    private static final Logger logger = LoggerFactory.getLogger(RemotePush.class);
+
+    //method push to push files to remote repo
+    public static void push(String path, String remoteUrl, String username, String password) {
+        // log info
+        logger.info("Pushing files to remote repo");
+        // push files to remote repo
+        try {
+            Git git = Git.open(new File(path));
+            git.push().setRemote(remoteUrl).setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password)).call();
+            // log info
+            logger.info("Pushed files to remote repo");
+        } catch (IOException | GitAPIException e) {
+            logger.error("Exception occurred while pushing files to remote repo", e);
+        }
+    }
+
+    //method pushAuth to push files to remote repo
+    public static void pushAuth(String path, String remoteUrl) {
+        // log info
+        logger.info("Pushing files to remote repo");
+        // push files to remote repo
+        try {
+            Git git = Git.open(new File(path));
+            git.push().setRemote(remoteUrl).call();
+            // log info
+            logger.info("Pushed files to remote repo");
+        } catch (IOException | GitAPIException e) {
+            logger.error("Exception occurred while pushing files to remote repo", e);
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        //test pushAuth method
+        pushAuth("D:\\00_Trainings\\CTS\\CoPilot\\prac\\testgit", "https://github.com/mdghouse-s/jgitdemo.git");
+        System.out.println("Done");
+    
+
+    }
+
+}
